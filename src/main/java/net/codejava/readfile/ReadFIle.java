1b62d16eca77ce78f6de2050431962a4da6b857f
Biplav Karki <valpib3@gmail.com>
2023-06-04 05:30:25 +1000

    A java project to read a text file

---
+++ src/main/java/net/codejava/readfile/ReadFIle.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package net.codejava.readfile;
+
+/**
+ *
+ * @author valpib
+ */
+public class ReadFIle {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
